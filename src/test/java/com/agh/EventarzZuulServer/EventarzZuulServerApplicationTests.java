8c422e6f6745f9a2659db98547f8d8b21bed01d8
Atloas <miter010@gmail.com>
2021-06-03 16:24:09 +0200

    Repo split

---
+++ src/test/java/com/agh/EventarzZuulServer/EventarzZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.EventarzZuulServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarzZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
